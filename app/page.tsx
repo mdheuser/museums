6cf3d55ae2155b5f29e182f3decc47d37fa4031f
Martin Dahlström-Heuser <martin.heuser@gmail.com>
2026-01-22 17:37:30 +0200

    corrected paragraph tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1>Welcome to Museums Project</h1>
       <p>This is built with Next.js + TypeScript</p>
-      TESTING AUTO UPDATE<p/>
+      <p>TESTING AUTO UPDATE</p>
     </div>
   );
 }
